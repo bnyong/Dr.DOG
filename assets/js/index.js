c4a3b4228d728ff49cdde769a558f9eff842eabc
bnyong <alex_9001@naver.com>
2025-05-10 13:33:38 +0900

    popup 관련 제거

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,3 @@
-// 팝업창 띄우기 함수생성
-
-function openPopup() {
-	$('#popup').fadeIn(function () {
-		// 검정배경 나타남
-		$('#popup .body').css('display', 'block'); // 팝업창내용 보여짐
-	});
-}
-
 $(document).ready(function () {
 	/* gnb 노출 */
 	$('.gnb-menu > li > a').mouseenter(function () {
